8d86e68858bb5ff515c10c8791642ce55fcf8050
Luciana Britos <luciana.dalma.britos@gmail.com>
2022-02-14 14:02:19 -0300

    modificamos interfas

--- src/components/Tasks/ListadoItem.js
+++ src/components/Tasks/ListadoItem.js
@@ -1,11 +1,23 @@
-const ListadoItem = ({nombre}) => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faPencil } from "@fortawesome/free-solid-svg-icons";
+
+const ListadoItem = ({id, nombre, handleDelete}) => {
     return (
-        <button key={nombre} 
-            type="button" 
-            className="list-group-item list-group-item-action" 
+        <div key={nombre} 
+            className="d-flex list-group-item list-group-item-action w-100 justify-content-evenly text-capitalize name-task" 
             aria-current="true">
-            {nombre}
-        </button>
+                <div className="c-grey fs-5">
+                    {nombre}
+                </div>
+                <div className="d-flex justify-content-end">
+                    <div className="mx-2"> 
+                        <FontAwesomeIcon icon={faPencil} />
+                    </div>
+                    <div className="mx-2" onClick={() => handleDelete(id)}>
+                        <FontAwesomeIcon icon={faTrash} />
+                    </div>
+                </div>
+        </div>
     );
 }
 
